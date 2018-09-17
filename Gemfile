6bc6b0501726016cbba8cc04cdba6468e6765d5f
Michael Isaakidis <michaelisaakidis@gmail.com>
2018-09-17 19:39:16 +0100

    Added cocoapods and initialised workspace

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 ruby "2.3.7"
-gem 'cocoapods'
-gem 'cocoapods-keys'
+gem 'cocoapods', '~> 1.5.3'
---
+++ Pods/Target Support Files/Pods-Popcorn NightUITests/Pods-Popcorn NightUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Popcorn_NightUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Popcorn_NightUITests
+@end
