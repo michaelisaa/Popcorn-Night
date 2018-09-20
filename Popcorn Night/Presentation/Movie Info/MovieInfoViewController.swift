67fcdef601e08d72c34cb23f19da25954d2cd70e
Michael Isaakidis <michaelisaakidis@gmail.com>
2018-09-20 22:12:40 +0100

    Improved movie details view

--- Popcorn Night/Presentation/Movie Info/MovieInfoViewController.swift
+++ Popcorn Night/Presentation/Movie Info/MovieInfoViewController.swift
@@ -9,44 +9,43 @@
 import UIKit
 import PureLayout
 
-class MovieInfoViewController: UIViewController {
-    let scrollView = UIScrollView(forAutoLayout: ())
-    let scrollContentView = UIView(forAutoLayout: ())
-    let overviewLabel = UILabel(forAutoLayout: ())
-    let genreLabel = UILabel(forAutoLayout: ())
-    let revenueLabel = UILabel(forAutoLayout: ())
+class MovieInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView = UITableView(forAutoLayout: ())
     let posterImageView = UIImageView(forAutoLayout: ())
     let emptyStateView = MovieListEmptyStateView(forAutoLayout: ())
-    let movieId: Int?
     var movie: Movie?
     let contentInset:CGFloat = 10
+    var availableMovieDetails = [MovieDetailsCellType]()
+    let cellIdentifier = "movieDetailsCell"
     
-    init(movieId: Int) {
-        self.movieId = movieId
+    init(movie: Movie) {
+        self.movie = movie
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        movieId = nil
+        movie = nil
         super.init(coder: aDecoder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        configureScrollView()
+        configureTableView()
         configureEmptyStateView()
+        configureNavbar()
         fetchMovieDetails()
     }
     
     func fetchMovieDetails() {
-        if let movieId = movieId {
+        if let movie = movie {
             emptyStateView.configure(state: .Loading)
             emptyStateView.isHidden = false
-            APICLient.getMovieDetails(movieId: movieId, success: { (movieInfo) in
+            APICLient.getMovieDetails(movieId: movie.movieId, success: { (movieInfo) in
                 self.movie = movieInfo
                 self.emptyStateView.isHidden = true
-                self.configureMovieInfoView()
+                self.configureMovieInfo()
             }) { (_) in
                 self.emptyStateView.configure(state: .Error)
                 self.emptyStateView.isHidden = false
@@ -59,14 +58,21 @@ class MovieInfoViewController: UIViewController {
     
     // MARK: - Configure Views
     
-    func configureMovieInfoView() {
-        configureTitleLabel()
+    func configureMovieInfo() {
         configurePosterImageView()
-        configureGenreLabel()
-        configureOverviewLabel()
-        configureReleaseAndRuntimeLabel()
-        configureRevenueLabel()
-        configureHomepageLinkeLabel()
+        availableMovieDetails.append(.Summary)
+        if movie?.genres != nil {
+            availableMovieDetails.append(.Genres)
+        }
+        availableMovieDetails.append(.Overview)
+        if let revenue = movie?.revenue, revenue > 0 {
+            availableMovieDetails.append(.Revenue)
+        }
+        if movie?.homepage != nil {
+            availableMovieDetails.append(.HomePageLink)
+        }
+        tableView.contentInset = UIEdgeInsetsMake(300, 0, 0, 0)
+        tableView.reloadData()
     }
     
     func configureEmptyStateView() {
@@ -76,58 +82,26 @@ class MovieInfoViewController: UIViewController {
         view.bringSubview(toFront: emptyStateView)
     }
     
-    func configureScrollView() {
-        view.addSubview(scrollView)
-        scrollView.autoPinEdgesToSuperviewEdges()
-        scrollView.addSubview(scrollContentView)
-        scrollContentView.autoPinEdgesToSuperviewEdges()
-        scrollContentView.autoMatch(.width, to: .width, of: scrollView, withOffset: 0)
-        scrollContentView.autoMatch(.height, to: .height, of: scrollView, withOffset: 0)
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.register(MovieDetailsCell.classForCoder(), forCellReuseIdentifier: cellIdentifier)
+        tableView.autoPinEdgesToSuperviewEdges()
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
     }
     
-    func configureTitleLabel() {
-//        view.addSubview(titleLabel)
-//        titleLabel.text = movie?.title
-//        titleLabel.numberOfLines = 0
-//        titleLabel.font = UIFont.systemFont(ofSize: 26, weight: .bold)
-//        titleLabel.textColor = .white
-//        titleLabel.autoPinEdge(toSuperviewSafeArea: .left)
-//        titleLabel.autoPinEdge(toSuperviewSafeArea: .right)
-//        titleLabel.autoPinEdge(toSuperviewSafeArea: .top)
+    func configureNavbar() {
         navigationItem.title = movie?.title
         navigationController?.navigationBar.prefersLargeTitles = false
     }
     
-    func configureGenreLabel() {
-        scrollContentView.addSubview(genreLabel)
-        genreLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        genreLabel.numberOfLines = 0
-        genreLabel.textColor = .black
-        genreLabel.autoPinEdge(.top, to: .bottom, of: posterImageView, withOffset: contentInset)
-        genreLabel.autoPinEdge(toSuperviewSafeArea: .left, withInset: contentInset)
-        genreLabel.autoPinEdge(toSuperviewSafeArea: .right, withInset: contentInset)
-        if let genres = movie?.genres {
-            genreLabel.text =  genres.map { (genre) -> String in
-                genre.name!
-            }.joined(separator: " | ")
-        }
-    }
-    
-    func configureOverviewLabel() {
-        scrollContentView.addSubview(overviewLabel)
-        overviewLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        overviewLabel.numberOfLines = 0
-        overviewLabel.textColor = .black
-        overviewLabel.autoPinEdge(toSuperviewSafeArea: .left, withInset: contentInset)
-        overviewLabel.autoPinEdge(toSuperviewSafeArea: .right, withInset: contentInset)
-        overviewLabel.autoPinEdge(.top, to: .bottom, of: genreLabel, withOffset: contentInset)
-        overviewLabel.text = movie?.overview
-    }
-    
     func configurePosterImageView() {
-        scrollContentView.addSubview(posterImageView)
+        view.addSubview(posterImageView)
         posterImageView.autoPinEdge(toSuperviewEdge: .left)
-        posterImageView.autoPinEdge(toSuperviewEdge: .top)
+        posterImageView.autoPinEdge(toSuperviewSafeArea: .top)
         posterImageView.autoPinEdge(toSuperviewEdge: .right)
         posterImageView.autoSetDimension(.height, toSize: 300)
         posterImageView.contentMode = .scaleAspectFill
@@ -146,74 +120,27 @@ class MovieInfoViewController: UIViewController {
         }
     }
     
-    func configureReleaseAndRuntimeLabel() {
-        let ratingsLabel = UILabel(forAutoLayout: ())
-        let releaseAndRuntimeLabel = UILabel(forAutoLayout: ())
-        let backgroundView = UIView(forAutoLayout: ())
-        backgroundView.backgroundColor = .black
-        backgroundView.alpha = 0.75
-        scrollContentView.addSubview(backgroundView)
-        backgroundView.autoPinEdge(toSuperviewEdge: .left)
-        backgroundView.autoPinEdge(toSuperviewEdge: .right)
-        backgroundView.autoPinEdge(.bottom, to: .bottom, of: posterImageView)
-        backgroundView.autoSetDimension(.height, toSize: 40)
-        var labelText = String(movie!.releaseDate.split(separator: "-").first!)
-        if  let runtime = movie!.runtime {
-            labelText = "\(labelText)  ∙  \(runtime) min"
-        }
-        
-        if let language = movie?.originalLanguage {
-            labelText = "\(labelText)  ∙  \(language.uppercased())"
-        }
-        releaseAndRuntimeLabel.text = labelText
-        releaseAndRuntimeLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        releaseAndRuntimeLabel.textColor = .white
-        backgroundView.addSubview(releaseAndRuntimeLabel)
-        backgroundView.addSubview(ratingsLabel)
-        releaseAndRuntimeLabel.autoPinEdgesToSuperviewSafeArea(with: UIEdgeInsetsMake(0, contentInset, 0, 0), excludingEdge: .right)
-        ratingsLabel.autoPinEdgesToSuperviewSafeArea(with: UIEdgeInsetsMake(0, 0, 0, contentInset), excludingEdge: .left)
-        ratingsLabel.autoPinEdge(.left, to: .right, of: releaseAndRuntimeLabel)
-        ratingsLabel.textAlignment = .right
-        ratingsLabel.textColor = .white
-        if let voteAverage = movie?.voteAverage, voteAverage > 0.0 {
-            ratingsLabel.text = "\(voteAverage) / 10 ⭐️"
-        }
-    }
-    
-    func configureRevenueLabel() {
-        scrollContentView.addSubview(revenueLabel)
-        revenueLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        revenueLabel.numberOfLines = 1
-        revenueLabel.textColor = .black
-        revenueLabel.autoPinEdge(toSuperviewSafeArea: .left, withInset: contentInset)
-        revenueLabel.autoPinEdge(toSuperviewSafeArea: .right, withInset: contentInset)
-        revenueLabel.autoPinEdge(.top, to: .bottom, of: overviewLabel, withOffset: contentInset)
-        if let revenue = movie?.revenue, revenue > 0 {
-            revenueLabel.text = "$\(revenue)"
-        }
-    }
-    
-    func configureHomepageLinkeLabel() {
-        
-        let homePageLinkTextView = UITextView(forAutoLayout: ())
-        homePageLinkTextView.isEditable = false
-        homePageLinkTextView.dataDetectorTypes = .link
-        scrollContentView.addSubview(homePageLinkTextView)
-        homePageLinkTextView.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        homePageLinkTextView.textColor = .black
-        homePageLinkTextView.tintColor = .blue
-        homePageLinkTextView.autoPinEdge(toSuperviewSafeArea: .left, withInset: contentInset)
-        homePageLinkTextView.autoPinEdge(toSuperviewSafeArea: .right, withInset: contentInset)
-        homePageLinkTextView.autoPinEdge(.top, to: .bottom, of: revenueLabel, withOffset: contentInset)
-        homePageLinkTextView.autoSetDimension(.height, toSize: 30)
-        if let homeLink = movie?.homepage {
-            homePageLinkTextView.text = homeLink
-        }
-    }
-    
     func urlForMoviePoster() -> URL? {
         guard let backdropPath = movie?.backdropPath else { return nil }
         let urlString = "https://image.tmdb.org/t/p/w780" + backdropPath
         return URL(string: urlString)
+    }
+    
+    // MARK: - Tableview
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return availableMovieDetails.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        guard let movieDetailsCell = cell as? MovieDetailsCell else {return cell}
+        movieDetailsCell.configure(movie: movie!, type: availableMovieDetails[indexPath.row])
+        return movieDetailsCell
     }
 }
